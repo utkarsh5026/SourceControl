47b2fd7a0efe3fc9355b4be29f7db6a0ec6694d3
Utkarsh Priyadarshi <utkarshpriyadarshi5026@gmail.com>
2025-08-04 13:26:17 +0530

    Add moduleNameMapper configuration to jest.config.js for simplified module imports

--- jest.config.js
+++ jest.config.js
@@ -6,6 +6,9 @@ module.exports = {
   transform: {
     '^.+\\.ts$': 'ts-jest',
   },
+  moduleNameMapper: {
+    '^@/(.*)$': '<rootDir>/src/$1',
+  },
   collectCoverageFrom: [
     'src/**/*.ts',
     '!src/**/*.d.ts',
