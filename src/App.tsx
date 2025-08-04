a48d61ca1d3b229909024d75ae12a022af2dc419
Utkarsh Priyadarshi <utkarshpriyadarshi5026@gmail.com>
2025-08-04 10:18:46 +0530

    Refactor App component to integrate DirectoryView and TextEditor
    This commit updates the App component by removing the previous layout and incorporating the DirectoryView and TextEditor components. It also adds a useEffect hook to apply a dark theme to the document body. The changes enhance the application's structure and user interface for file management and editing.

--- src/App.tsx
+++ src/App.tsx
@@ -1,35 +1,22 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+import DirectoryView from "./components/directory/components/directory-view";
+import TextEditor from "./components/text-editor/components/text-editor";
+import { useEffect } from "react";
 
-function App() {
-  const [count, setCount] = useState(0)
+const App = () => {
+  useEffect(() => {
+    document.body.classList.add("dark");
+  }, []);
 
   return (
-    <>
-      <div>
-        <a href="https://vite.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
+    <div className="h-screen flex bg-background">
+      <div className="w-80 border-r ">
+        <DirectoryView />
       </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.tsx</code> and save to test HMR
-        </p>
+      <div className="flex-1">
+        <TextEditor />
       </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </>
-  )
-}
+    </div>
+  );
+};
 
-export default App
+export default App;
