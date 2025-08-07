705a65c2c37f2f926ac0367df25905df8f2ef4ff
Utkarsh Priyadarshi <utkarshpriyadarshi5026@gmail.com>
2025-08-07 15:39:13 +0530

    Add write-tree command to CLI and update command exports
    This commit introduces the `write-tree` command, allowing users to create a tree object from the current working directory. The command includes options for specifying a subdirectory and excluding certain directories, enhancing flexibility. Additionally, the command is integrated into the CLI and exported from the commands index, improving the overall functionality and user experience of the CLI.

--- src/commands/index.ts
+++ src/commands/index.ts
@@ -3,5 +3,13 @@ import { catFileCommand } from './cat-file';
 import { initCommand } from './init';
 import { destroyCommand } from './destroy';
 import { lsTreeCommand } from './ls-tree';
+import { writeTreeCommand } from './write-tree';
 
-export { hashObjectCommand, catFileCommand, initCommand, destroyCommand, lsTreeCommand };
+export {
+  hashObjectCommand,
+  catFileCommand,
+  initCommand,
+  destroyCommand,
+  lsTreeCommand,
+  writeTreeCommand,
+};
