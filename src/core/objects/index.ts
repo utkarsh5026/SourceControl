0d2affb2700124053c2bf391d5d57fe4b020b52c
Utkarsh Priyadarshi <utkarshpriyadarshi5026@gmail.com>
2025-08-07 12:05:45 +0530

    Add CommitObject export to core objects index
    This commit updates the core objects index to include the CommitObject in the exported module. This change enhances the object model by making the CommitObject readily available for use in other parts of the application, facilitating better management of Git commit representations.

--- src/core/objects/index.ts
+++ src/core/objects/index.ts
@@ -1,6 +1,7 @@
 import { BlobObject } from './blob/blob-object';
 import { TreeObject } from './tree/tree-object';
+import { CommitObject } from './commit/commit-object';
 import { GitObject } from './base';
 import { ObjectType } from './base';
 
-export { BlobObject, TreeObject, GitObject, ObjectType };
+export { BlobObject, TreeObject, GitObject, ObjectType, CommitObject };
