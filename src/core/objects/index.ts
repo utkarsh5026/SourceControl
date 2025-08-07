a28900b17c294d31bf3f1f4043d507aab8fe15d5
Utkarsh Priyadarshi <utkarshpriyadarshi5026@gmail.com>
2025-08-07 08:50:59 +0530

    Update index.ts to export TreeObject, GitObject, and ObjectType
    This commit modifies the index.ts file to include exports for TreeObject, GitObject, and ObjectType, enhancing the module's accessibility and organization. This change allows for easier imports of these classes in other parts of the application, streamlining the development process.

--- src/core/objects/index.ts
+++ src/core/objects/index.ts
@@ -1,3 +1,6 @@
 import { BlobObject } from './blob/blob-object';
+import { TreeObject } from './tree/tree-object';
+import { GitObject } from './base';
+import { ObjectType } from './base';
 
-export { BlobObject };
+export { BlobObject, TreeObject, GitObject, ObjectType };
