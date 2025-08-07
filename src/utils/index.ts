6a01481c823feeb642f38286cf73daececa8f3e3
Utkarsh Priyadarshi <utkarshpriyadarshi5026@gmail.com>
2025-08-07 09:32:23 +0530

    Update index.ts to include logger export for improved logging capabilities
    This commit modifies the index.ts file to export the logger utility, enhancing the module's functionality by providing a centralized logging mechanism. This addition streamlines logging across the application, improving maintainability and debugging processes.

--- src/utils/index.ts
+++ src/utils/index.ts
@@ -1,5 +1,6 @@
 import { FileUtils } from './file';
 import { HashUtils } from './hash';
 import { CompressionUtils } from './compress';
+import { logger } from './logger';
 
-export { FileUtils, HashUtils, CompressionUtils };
+export { FileUtils, HashUtils, CompressionUtils, logger };
