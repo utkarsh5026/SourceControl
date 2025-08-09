a8cf1ce0525bfcf14b7adf7b2eb39e200fb66565
Utkarsh Priyadarshi <utkarshpriyadarshi5026@gmail.com>
2025-08-09 16:52:45 +0530

    Update index.ts to export ConfigStore class for enhanced configuration management

--- src/core/config/index.ts
+++ src/core/config/index.ts
@@ -1,4 +1,5 @@
 import { ConfigLevel, ConfigEntry } from './config-level';
 import { ConfigParser } from './config-parser';
+import { ConfigStore } from './config-store';
 
-export { ConfigLevel, ConfigEntry, ConfigParser };
+export { ConfigLevel, ConfigEntry, ConfigParser, ConfigStore };
