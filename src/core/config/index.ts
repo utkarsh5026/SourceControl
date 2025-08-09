3f2ba9a8742f54f28dd48bbac7b8d6979c2229e7
Utkarsh Priyadarshi <utkarshpriyadarshi5026@gmail.com>
2025-08-10 01:55:57 +0530

    Enhance config module by exporting GitConfigManager and TypedConfig
    This commit updates the index.ts file in the config module to export the newly introduced GitConfigManager and TypedConfig classes. This enhancement improves the accessibility of configuration management tools, facilitating better integration and usage within the codebase.

--- src/core/config/index.ts
+++ src/core/config/index.ts
@@ -1,5 +1,7 @@
 import { ConfigLevel, ConfigEntry } from './config-level';
 import { ConfigParser } from './config-parser';
 import { ConfigStore } from './config-store';
+import { GitConfigManager } from './config-manager';
+import { TypedConfig } from './typed-config';
 
-export { ConfigLevel, ConfigEntry, ConfigParser, ConfigStore };
+export { ConfigLevel, ConfigEntry, ConfigParser, ConfigStore, GitConfigManager, TypedConfig };
