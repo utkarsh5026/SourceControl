9dd329834624edd8dfb028c499d433ad82f70e7b
Utkarsh Priyadarshi <utkarshpriyadarshi5026@gmail.com>
2025-08-10 07:11:46 +0530

    Update index.ts to export TreeWalker class for enhanced Git tree traversal functionality

--- src/core/index/index.ts
+++ src/core/index/index.ts
@@ -1,5 +1,6 @@
 import { GitIndex } from './git-index';
 import { IndexManager } from './index-manager';
 import type { AddOptions, AddResult, RemoveResult, StatusResult } from './types';
+import { TreeWalker } from './tree-walker';
 
-export { GitIndex, IndexManager, AddOptions, AddResult, RemoveResult, StatusResult };
+export { GitIndex, IndexManager, AddOptions, AddResult, RemoveResult, StatusResult, TreeWalker };
