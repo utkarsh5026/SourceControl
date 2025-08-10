50fcf06b3c005ffd4f7e844c9d67f5f8a8f6a438
Utkarsh Priyadarshi <utkarshpriyadarshi5026@gmail.com>
2025-08-10 11:16:37 +0530

    Refactor index.ts to consolidate imports from base module
    This commit updates the index.ts file to import ObjectTypeHelper alongside GitObject and ObjectType from the base module. The export statement has also been modified to include ObjectTypeHelper, improving the organization and accessibility of object types within the codebase.

--- src/core/objects/index.ts
+++ src/core/objects/index.ts
@@ -1,7 +1,6 @@
 import { BlobObject } from './blob/blob-object';
 import { TreeObject } from './tree/tree-object';
 import { CommitObject } from './commit/commit-object';
-import { GitObject } from './base';
-import { ObjectType } from './base';
+import { GitObject, ObjectType, ObjectTypeHelper } from './base';
 
-export { BlobObject, TreeObject, GitObject, ObjectType, CommitObject };
+export { BlobObject, TreeObject, GitObject, ObjectType, ObjectTypeHelper, CommitObject };
