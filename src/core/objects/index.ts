4b213bb3efdc9c2c53d0dc3b6366a111e2ea2bfb
Utkarsh Priyadarshi <utkarshpriyadarshi5026@gmail.com>
2025-08-11 05:14:52 +0530

    Enhance object exports in index.ts by adding CommitPerson
    This commit updates the exports in index.ts to include the CommitPerson class, improving the accessibility of commit-related functionalities within the codebase. This change enhances the overall organization and usability of the module, allowing for better integration with other components related to commit management.

--- src/core/objects/index.ts
+++ src/core/objects/index.ts
@@ -1,6 +1,15 @@
 import { BlobObject } from './blob/blob-object';
 import { TreeObject } from './tree/tree-object';
 import { CommitObject } from './commit/commit-object';
+import { CommitPerson } from './commit/commit-person';
 import { GitObject, ObjectType, ObjectTypeHelper } from './base';
 
-export { BlobObject, TreeObject, GitObject, ObjectType, ObjectTypeHelper, CommitObject };
+export {
+  BlobObject,
+  TreeObject,
+  GitObject,
+  ObjectType,
+  ObjectTypeHelper,
+  CommitObject,
+  CommitPerson,
+};
