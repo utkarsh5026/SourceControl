388e0e9beae3d0f7e4302933df3e7e03f4cd1069
Utkarsh Priyadarshi <utkarshpriyadarshi5026@gmail.com>
2025-08-11 10:29:33 +0530

    Update index.ts to export WorkingDirectoryManager alongside existing branch management exports

--- src/core/branch/index.ts
+++ src/core/branch/index.ts
@@ -1,4 +1,5 @@
 import { BranchManager } from './branch-manager';
+import { WorkingDirectoryManager } from './workdir-manager';
 import type { BranchInfo, CreateBranchOptions, CheckoutOptions } from './types';
 
-export { BranchManager, BranchInfo, CreateBranchOptions, CheckoutOptions };
+export { BranchManager, BranchInfo, CreateBranchOptions, CheckoutOptions, WorkingDirectoryManager };
