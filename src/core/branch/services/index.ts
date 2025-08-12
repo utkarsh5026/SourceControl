767fbc565dc2e67ed1c2d2dc2f57f704c95a312b
Utkarsh Priyadarshi <utkarshpriyadarshi5026@gmail.com>
2025-08-12 06:53:05 +0530

    Add index.ts file to export branch-related services
    This commit introduces a new index.ts file that consolidates and exports the BranchCreator, BranchInfoService, BranchRefService, and BranchValidator classes. This enhancement improves the organization and accessibility of branch management functionalities within the codebase, facilitating easier imports for other modules.

---
+++ src/core/branch/services/index.ts
@@ -0,0 +1,6 @@
+import { BranchCreator } from './branch-creation';
+import { BranchInfoService } from './branch-info';
+import { BranchRefService } from './branch-ref';
+import { BranchValidator } from './branch-validator';
+
+export { BranchCreator, BranchInfoService, BranchRefService, BranchValidator };
