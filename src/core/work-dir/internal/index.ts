f75fec56a0644dbc752047d53c47b2aabe8c174e
Utkarsh Priyadarshi <utkarshpriyadarshi5026@gmail.com>
2025-08-12 14:36:23 +0530

    Import TreeAnalyzer in index.ts to enhance file operation exports

--- src/core/work-dir/internal/index.ts
+++ src/core/work-dir/internal/index.ts
@@ -1,5 +1,6 @@
 import { WorkingDirectoryValidator } from './workingdir-validator';
 import { FileOperationService } from './file-operation';
+import { TreeAnalyzer } from './tree-analyzer';
 import type { FileOperation, FileBackup } from './types';
 
-export { WorkingDirectoryValidator, FileOperationService, FileBackup, FileOperation };
+export { WorkingDirectoryValidator, FileOperationService, FileBackup, FileOperation, TreeAnalyzer };
