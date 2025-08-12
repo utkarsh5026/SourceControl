5dba0e8aeb9a398ededfe5150793a4933a04b505
Utkarsh Priyadarshi <utkarshpriyadarshi5026@gmail.com>
2025-08-12 06:43:44 +0530

    Add Queue class for managing a collection of items
    This commit introduces a new Queue class that provides methods for managing a collection of items, including push and shift operations. The Queue class maintains an internal store and optimizes memory usage by trimming the store when the head exceeds a certain threshold. Additionally, the index.ts file is updated to export the Queue class, enhancing the utility functions available in the codebase.

--- src/utils/index.ts
+++ src/utils/index.ts
@@ -3,5 +3,6 @@ import { HashUtils } from './helpers/hash';
 import { CompressionUtils } from './helpers/compress';
 import { logger } from './cli/logger';
 import { display } from './cli/display';
+import { Queue } from './helpers/queue';
 
-export { FileUtils, HashUtils, CompressionUtils, logger, display };
+export { FileUtils, HashUtils, CompressionUtils, logger, display, Queue };
