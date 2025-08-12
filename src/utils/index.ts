21b14b33bd3cd6643dc473b8f10b699425be98b2
Utkarsh Priyadarshi <utkarshpriyadarshi5026@gmail.com>
2025-08-12 14:36:45 +0530

    Enhance exports in index.ts by adding PathUtils for improved path handling

--- src/utils/index.ts
+++ src/utils/index.ts
@@ -1,8 +1,9 @@
 import { FileUtils } from './io/file';
+import { PathUtils } from './io/path';
 import { HashUtils } from './helpers/hash';
 import { CompressionUtils } from './helpers/compress';
 import { logger } from './cli/logger';
 import { display } from './cli/display';
 import { Queue } from './helpers/queue';
 
-export { FileUtils, HashUtils, CompressionUtils, logger, display, Queue };
+export { FileUtils, HashUtils, CompressionUtils, logger, display, Queue, PathUtils };
