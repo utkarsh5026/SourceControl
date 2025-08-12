adcbae005c2dcfec751bcf690552687bd68e2ab1
Utkarsh Priyadarshi <utkarshpriyadarshi5026@gmail.com>
2025-08-12 06:17:58 +0530

    Refactor ObjectValidator methods to handle null objects and centralize type checking
    This commit updates the ObjectValidator class by modifying the isCommit, isTree, and isBlob methods to accept null objects, enhancing type safety. Additionally, a new private method, isObjectType, is introduced to centralize the logic for checking object types, improving code maintainability and readability.

--- src/core/objects/utils/validate.ts
+++ src/core/objects/utils/validate.ts
@@ -12,21 +12,32 @@ export class ObjectValidator {
   /**
    * Checks if the object is a commit.
    */
-  public static isCommit(object: GitObject): object is CommitObject {
-    return object && object.type() === ObjectType.COMMIT;
+  public static isCommit(object: GitObject | null): object is CommitObject {
+    return ObjectValidator.isObjectType(object, ObjectType.COMMIT);
   }
 
   /**
    * Checks if the object is a tree.
    */
-  public static isTree(object: GitObject): object is TreeObject {
-    return object && object.type() === ObjectType.TREE;
+  public static isTree(object: GitObject | null): object is TreeObject {
+    return ObjectValidator.isObjectType(object, ObjectType.TREE);
   }
 
   /**
    * Checks if the object is a blob.
    */
-  public static isBlob(object: GitObject): object is BlobObject {
-    return object && object.type() === ObjectType.BLOB;
+  public static isBlob(object: GitObject | null): object is BlobObject {
+    return ObjectValidator.isObjectType(object, ObjectType.BLOB);
+  }
+
+  /**
+   * Checks if the object is of a specific type.
+   */
+  private static isObjectType<T extends GitObject>(
+    object: GitObject | null,
+    type: ObjectType
+  ): object is T {
+    if (!object) return false;
+    return object.type() === type;
   }
 }
