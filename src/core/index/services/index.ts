f27b84c8a3a9ab67453fcd2861ea317ee662b17f
Utkarsh Priyadarshi <utkarshpriyadarshi5026@gmail.com>
2025-08-13 12:33:00 +0530

    Enhance exports in index.ts by adding IndexEntryComparator for improved index entry comparison functionality

--- src/core/index/services/index.ts
+++ src/core/index/services/index.ts
@@ -1,4 +1,5 @@
 import { IndexFileAdder } from './index-file-adder';
 import { StatusCalculator } from './status-calculator';
+import { IndexEntryComparator } from './index-entry-comparator';
 
-export { IndexFileAdder, StatusCalculator };
+export { IndexFileAdder, StatusCalculator, IndexEntryComparator };
